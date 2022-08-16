788ee047d816c476091601bac93bad2885da2630
Kunal <kunalsheth1177@gmail.com>
2022-08-16 13:56:58 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import Navbar from "./Navbar"
+import Contact from "./pages/Contact"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import { Route, Routes } from "react-router-dom"
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Contact" element={<Contact />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </>
+  )
+}
+
+export default App
